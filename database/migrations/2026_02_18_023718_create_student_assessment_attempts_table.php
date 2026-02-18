18fe30b72ed2d5e947db754439544fa518a68ac0
raves21 <johnraves23@gmail.com>
2026-02-18 16:57:55 +0800

    dkalsdsa

---
+++ database/migrations/2026_02_18_023718_create_student_assessment_attempts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_assessment_attempts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('student_id')->constrained()->cascadeOnDelete();
+            $table->foreignUuid('assessment_id')->constrained()->cascadeOnDelete();
+            $table->integer('attempt_number');
+            $table->enum('status', ['ongoing', 'submitted']);
+            $table->timestamp('submitted_at')->nullable();
+            $table->double('score')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_assessment_attempts');
+    }
+};
