f7a8edf5af7ccdda35581ed0d11a06c5f272488b
raves21 <johnraves23@gmail.com>
2026-06-27 01:28:35 +0800

    updated composer.lock

--- app/Http/Repositories/AssessmentResultRepository.php
+++ app/Http/Repositories/AssessmentResultRepository.php
@@ -46,4 +46,78 @@ class AssessmentResultRepository extends BaseRepository
             })->toArray()
         ];;
     }
+
+    public function getAllByAssessment(string $assessmentId)
+    {
+
+        $results = AssessmentResult::with(['student.user', 'student.program', 'assessment.chapterContent'])->where('assessment_id', $assessmentId)->get();
+
+        return $results->map(function ($asmtResult) use ($assessmentId) {
+            $attempts = StudentAssessmentAttempt::whereHas('assessmentVersion', function ($q) use ($assessmentId) {
+                $q->where('assessment_id', $assessmentId);
+            })->get();
+
+            $student = $asmtResult->student;
+
+            return [
+                'id' => $asmtResult->id,
+                'assessmentId' => $assessmentId,
+                'student' => [
+                    'id' => $student->id,
+                    'user' => [
+                        'id' => $student->user->id,
+                        'firstName' => $student->user->first_name,
+                        'lastName' => $student->user->last_name
+                    ],
+                    'program' => [
+                        'id' => $student->program->id,
+                        'name' => $student->program->name,
+                        'code' => $student->program->code,
+                    ]
+                ],
+                'totalAttempts' => $attempts->count(),
+                'finalScore' => $asmtResult->final_score,
+            ];
+        })->toArray();
+    }
+
+    public function getAttemptsByAssessmentResult(string $assessmentResultId)
+    {
+        $asmtResult = AssessmentResult::findOrFail($assessmentResultId)->load(['assessment', 'student']);
+
+
+        $assessment = $asmtResult->assessment;
+        $student = $asmtResult->student;
+
+        $attempts = StudentAssessmentAttempt::with(['assessmentVersion', 'student'])->whereHas('assessmentVersion', function ($q) use ($assessment, $student) {
+            $q->where('assessment_id', $assessment->id)
+                ->where('student_id', $student->id);
+        })->get();
+
+        return $attempts->map(function ($attempt) use ($student) {
+            return [
+                'id' => $attempt->id,
+                'student' => [
+                    'id' => $student->id,
+                    'user' => [
+                        'id' => $student->user->id,
+                        'firstName' => $student->user->first_name,
+                        'lastName' => $student->user->last_name
+                    ],
+                    'program' => [
+                        'id' => $student->program->id,
+                        'name' => $student->program->name,
+                        'code' => $student->program->code,
+                    ]
+                ],
+                'questionnaireSnapshot' => $attempt->assessmentVersion->questionnaire_snapshot,
+                'submissionSummary' => $attempt->submission_summary,
+                'status' => $attempt->status,
+                'attemptNumber' => $attempt->attempt_number,
+                'startedAt' => (string) $attempt->started_at,
+                'submittedAt' => (string) $attempt->submitted_at,
+                'totalScore' => $attempt->total_score,
+            ];
+        })->toArray();
+    }
 }
